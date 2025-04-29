a8f9da8f4fb267e34adbecae53712b7255adcf56
helitoo <24520145@gm.uit.edu.vn>
2025-04-29 21:23:09 +0700

    Fix refs

--- src/script/action.js
+++ src/script/action.js
@@ -114,175 +114,181 @@ document.addEventListener("DOMContentLoaded", () => {
       giftFolder.file("references.bib", prefContent);
       giftFolder.file(
         "main.tex",
-        `\\documentclass{set-up-page}
-
-\\usepackage[backend=biber,style=ieee,sorting=none,style=numeric-comp]{biblatex}
-\\addbibresource{references.bib}
-
-\\begin{document}
-
-\\coverpage
-
-\\input{content}
-
-\\setlength{\\parskip}{0pt}
-
-\\end{document}
-`
-      );
-      giftFolder.file(
-        "set-up-page.cls",
         `
-        \\NeedsTeXFormat{LaTeX2e}
 
-        \\LoadClass[12pt, a4paper, fleqn]{article}
-        
-        % Packages
-        \\RequirePackage[utf8]{vietnam}
-        \\RequirePackage{lmodern}
-        \\RequirePackage{graphicx}
-        \\RequirePackage{geometry}
-        \\RequirePackage{tikz}
-        \\usetikzlibrary{calc}
-        \\RequirePackage{ragged2e}
-        \\RequirePackage{setspace}
-        \\RequirePackage{datetime}
-        \\RequirePackage{scrextend}
-        \\RequirePackage{ifthen}
-        \\RequirePackage{hyperref}
-        \\RequirePackage{xstring}
-        \\usepackage{caption}
-        \\usepackage{float}
-        \\usepackage{fancyhdr}
-        \\usepackage{tabularx}
-        \\usepackage{csquotes}
-        \\usepackage{setspace}
-        
-        \\usepackage{indentfirst}
-        \\setlength{\\parindent}{1cm}
-        \\setlength{\\parskip}{0.3cm}
-        
-        \\fancyhf{}
-        
-        % Margins configurations
-        \\geometry{
-            a4paper,
-            left=3.5cm,
-            top=3cm,
-            right=2cm,
-            bottom=3.5cm
-        }
-        
-        % Font size
-        \\newcommand{\\SetCustomFontSize}{\\changefontsizes{13pt}}
-        \\AtBeginDocument{\\SetCustomFontSize}
-        
-        % line spacing
-        \\AtBeginDocument{\\setstretch{1.5}}
-        
-        \\usepackage{titlesec}
-        
-        % Pseudo section lv 1
-        \\newcommand{\\pseudoHeading}[1]{
-            \\clearpage
-            \\thispagestyle{empty}
-            \\begin{center}
-                \\textbf{#1}
-            \\end{center}
-        }
-        
-        % Uncount section lv 1
-        \\newcommand{\\uncountHeading}[1]{
-            \\clearpage
-            \\thispagestyle{empty}
-            \\section*{#1}
-            \\addcontentsline{toc}{section}{#1}
-        }
-        
-        % Secsion lv 1
-        \\setcounter{section}{1}
-        
-        \\titlespacing*{\\section}{0pt}{0pt}{10pt}
-        \\titleformat{\\section}{\\fontsize{14pt}{0pt}\\bfseries \\centering}{}{0pt}{\\setcounter{subsection}{0} \\setcounter{subsubsection}{0} \\setcounter{paragraph}{0}}
-        
-        \\newcommand{\\heading}[1]{
-            \\clearpage
-            \\vspace*{0.05cm}
-            \\section*{#1}
-            \\addcontentsline{toc}{section}{#1}
-        }
-        
-        % Secsion lv 2
-        \\titlespacing*{\\subsection}{0pt}{10pt}{0pt}
-        \\titleformat*{\\subsection}{\\fontsize{13pt}{0pt}\\bfseries}
-        
-        \\newcommand{\\subheading}[1]{
-            \\setcounter{subsubsection}{0} \\setcounter{paragraph}{0}
-            \\subsection{#1}
-        }
-        
-        % Secsion lv 3
-        \\titlespacing*{\\subsubsection}{0pt}{10pt}{0pt}
-        \\titleformat*{\\subsubsection}{\\fontsize{13pt}{0pt}\\bfseries \\itshape}
-        
-        \\newcommand{\\subsubheading}[1]{
-            \\subsubsection{#1}
-        }
-        
-        % Table of contents
-        \\makeatletter
-        \\renewcommand{\\tableofcontents}{
-            \\begingroup
-            \\pagenumbering{arabic}
-            \\setcounter{page}{0}
-            \\uncountHeading{MỤC LỤC}
-            \\renewcommand{\\contentsname}{}
-            \\thispagestyle{empty}
-            \\parindent 0pt
-            \\parskip 0pt
-            \\@starttoc{toc}
-            \\clearpage
-            \\endgroup
-        }
-        \\makeatother
-        
-        % Table of figures configure
-        \\makeatletter
-        \\renewcommand{\\listoffigures}{
-            \\begingroup
-            \\uncountHeading{DANH MỤC HÌNH ẢNH}
-            \\thispagestyle{empty}
-            \\parindent 0pt
-            \\parskip 0pt
-            \\@starttoc{lof}
-            \\clearpage
-            \\endgroup
-        }
-        \\makeatother
-        
-        % Table of tables configure
-        \\makeatletter
-        \\renewcommand{\\listoftables}{
-            \\begingroup
-            \\uncountHeading{DANH MỤC BẢNG BIỂU}
-            \\thispagestyle{empty}
-            \\parindent 0pt
-            \\parskip 0pt
-            \\@starttoc{lot}
-            \\clearpage
-            \\endgroup
-        }
-        \\makeatother
-        
-        % Figures configure
-        \\renewcommand{\\figurename}{\\fontsize{12pt}{0pt}\\selectfont \\bfseries Hình}
-        \\renewcommand{\\thefigure}{\\thesection.\\arabic{figure}}
-        \\captionsetup[figure]{labelsep=space}
-        
-        % Tables configure
-        \\renewcommand{\\tablename}{\\fontsize{12pt}{0pt}\\selectfont \\bfseries Bảng}
-        \\renewcommand{\\thetable}{\\thesection.\\arabic{table}}
-        \\captionsetup[table]{labelsep=space}
+\\NeedsTeXFormat{LaTeX2e}
+
+\\LoadClass[12pt, a4paper, fleqn]{article}
+
+% Packages
+\\RequirePackage[utf8]{vietnam}
+\\RequirePackage{lmodern}
+\\RequirePackage{graphicx}
+\\RequirePackage{geometry}
+\\RequirePackage{tikz}
+\\usetikzlibrary{calc}
+\\RequirePackage{ragged2e}
+\\RequirePackage{setspace}
+\\RequirePackage{datetime}
+\\RequirePackage{scrextend}
+\\RequirePackage{ifthen}
+\\RequirePackage{hyperref}
+\\RequirePackage{xstring}
+\\usepackage{caption}
+\\usepackage{float}
+\\usepackage{fancyhdr}
+\\usepackage{tabularx}
+\\usepackage{csquotes}
+\\usepackage{setspace}
+\\usepackage{mdframed}
+\\usepackage{multirow}
+\\usepackage{booktabs}
+\\usepackage{enumitem}
+\\usepackage{makecell}
+
+\\usepackage{enumitem}
+\\setlist{itemsep=0pt, topsep=0pt}
+\\usepackage{enumitem}
+\\setlist[itemize]{leftmargin=5em}
+
+\\usepackage{indentfirst}
+\\setlength{\\parindent}{1cm}
+\\setlength{\\parskip}{0.3cm}
+
+\\fancyhf{}
+
+% Margins configurations
+\\geometry{
+    a4paper,
+    left=3.5cm,
+    top=3cm,
+    right=2cm,
+    bottom=3.5cm
+}
+
+% Font size
+\\newcommand{\\SetCustomFontSize}{\\changefontsizes{13pt}}
+\\AtBeginDocument{\\SetCustomFontSize}
+
+% line spacing
+\\AtBeginDocument{\\setstretch{1.5}}
+
+\\usepackage{titlesec}
+
+% Pseudo section lv 1
+\\newcommand{\\pseudoHeading}[1]{
+    \\clearpage
+    \\thispagestyle{empty}
+    \\begin{center}
+        \\textbf{#1}
+    \\end{center}
+}
+
+% Uncount section lv 1
+\\newcommand{\\uncountHeading}[1]{
+    \\clearpage
+    \\thispagestyle{empty}
+    \\section*{#1}
+    \\addcontentsline{toc}{section}{#1}
+}
+
+% Secsion lv 1
+\\setcounter{section}{0}
+
+\\titlespacing*{\\section}{0pt}{0pt}{10pt}
+\\titleformat{\\section}{\\fontsize{14pt}{0pt}\\bfseries \\centering}{}{0pt}{\\setcounter{subsection}{0} \\setcounter{subsubsection}{0}}
+
+\\newcommand{\\heading}[1]{
+    \\clearpage
+    \\vspace*{0.05cm}
+    \\stepcounter{section}
+    \\section*{#1}
+    \\addcontentsline{toc}{section}{#1}
+}
+
+% Secsion lv 2
+\\titlespacing*{\\subsection}{0pt}{10pt}{0pt}
+\\titleformat*{\\subsection}{\\fontsize{14pt}{0pt}\\bfseries}
+
+\\newcommand{\\subheading}[1]{
+    \\setcounter{subsubsection}{0}
+    \\subsection{#1}
+}
+
+% Secsion lv 3
+\\titlespacing*{\\subsubsection}{0pt}{10pt}{0pt}
+\\titleformat*{\\subsubsection}{\\fontsize{13pt}{0pt}\\bfseries \\itshape}
+
+\\newcommand{\\subsubheading}[1]{
+    \\subsubsection{#1}
+}
+
+% Table of contents
+\\makeatletter
+\\renewcommand{\\tableofcontents}{
+    \\begingroup
+    \\pagenumbering{arabic}
+    \\setcounter{page}{0}
+    \\uncountHeading{MỤC LỤC}
+    \\renewcommand{\\contentsname}{}
+    \\thispagestyle{empty}
+    \\parindent 0pt
+    \\parskip -0.8pt
+    \\@starttoc{toc}
+    \\clearpage
+    \\endgroup
+}
+\\makeatother
+
+% Table of figures configure
+\\makeatletter
+\\renewcommand{\\listoffigures}{
+    \\begingroup
+    \\uncountHeading{DANH MỤC HÌNH ẢNH}
+    \\thispagestyle{empty}
+    \\parindent 0pt
+    \\parskip 0pt
+    \\@starttoc{lof}
+    \\clearpage
+    \\endgroup
+}
+\\makeatother
+
+% Table of tables configure
+\\makeatletter
+\\renewcommand{\\listoftables}{
+    \\begingroup
+    \\uncountHeading{DANH MỤC BẢNG BIỂU}
+    \\thispagestyle{empty}
+    \\parindent 0pt
+    \\parskip 0pt
+    \\@starttoc{lot}
+    \\clearpage
+    \\endgroup
+}
+\\makeatother
+
+% Table of tables references
+\\newcommand{\\refs}{
+  \\uncountHeading{DANH MỤC TÀI LIỆU THAM KHẢO}
+  \\thispagestyle{empty}
+  \\nocite{*}
+  \\printbibliography[heading=none]
+}
+
+\\usepackage{chngcntr}
+\\counterwithin{figure}{section}
+\\counterwithin{table}{section}
+
+% Figures configure
+\\renewcommand{\\figurename}{\\fontsize{12pt}{0pt}\\selectfont \\bfseries Hình}
+\\renewcommand{\\thefigure}{\\thesection.\\arabic{figure}}
+\\captionsetup[figure]{labelsep=space}
+
+% Tables configure
+\\renewcommand{\\tablename}{\\fontsize{12pt}{0pt}\\selectfont \\bfseries Bảng}
+\\renewcommand{\\thetable}{\\thesection.\\arabic{table}}
+\\captionsetup[table]{labelsep=space}
 
 ` + titlepageContent
       );
